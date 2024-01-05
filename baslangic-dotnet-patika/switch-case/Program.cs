78e296a6df17ed727645085681a2b1bd3d5222c2
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-01-05 14:50:26 +0300

    switch case

---
+++ baslangic-dotnet-patika/switch-case/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındayız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındayız");
+                    break;
+                case 3:
+                    Console.WriteLine("Nisan ayındayız");
+                    break;
+                case 4:
+                    Console.WriteLine("Mart ayındayız");
+                    break;
+                default:
+                    Console.WriteLine("YAnlış veri girişi");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                default:
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+
+            }
+        }
+
+    }
+}
