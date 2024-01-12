2e0b6073aa99f25736a035c65377289936eb1b04
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-01-12 16:18:27 +0300

    karakter yer degistirme

---
+++ baslangic-dotnet-patika/csharp-projects/karakter-yer-degistirme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir cümle girin:");
+        string input = Console.ReadLine();
+
+        string result = KarakterleriYerDegistir(input);
+        Console.WriteLine("Output: " + result);
+    }
+
+    static string KarakterleriYerDegistir(string input)
+    {
+        char[] charArray = input.ToCharArray();
+
+        for (int i = 1; i < charArray.Length; i += 2)
+        {
+            char temp = charArray[i - 1];
+            charArray[i - 1] = charArray[i];
+            charArray[i] = temp;
+        }
+
+        return new string(charArray);
+    }
+}
