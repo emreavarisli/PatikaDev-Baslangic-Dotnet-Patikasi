60532a12ab2b890234cd9adc94e2bcd56a83aa55
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-01-12 16:12:01 +0300

    daire cizme

---
+++ baslangic-dotnet-patika/csharp-projects/daire-cizme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Dairenin yarıçapını girin:");
+
+        if (double.TryParse(Console.ReadLine(), out double yaricap) && yaricap > 0)
+        {
+            DaireCizici.DaireCiz(yaricap);
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz giriş. Lütfen pozitif bir sayı girin.");
+        }
+    }
+}
+
+class DaireCizici
+{
+    public static void DaireCiz(double yaricap)
+    {
+        double cevre = CevreHesapla(yaricap);
+        double alan = AlanHesapla(yaricap);
+
+        Console.WriteLine($"Daire Çizildi - Çevre: {cevre}, Alan: {alan}");
+    }
+
+    private static double CevreHesapla(double yaricap)
+    {
+        return 2 * Math.PI * yaricap;
+    }
+
+    private static double AlanHesapla(double yaricap)
+    {
+        return Math.PI * Math.Pow(yaricap, 2);
+    }
+}
