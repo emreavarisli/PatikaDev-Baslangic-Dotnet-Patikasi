ada5956fb389c54cacda87cd3cf376ae1e6357a1
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-01-13 13:02:11 +0300

    integer ikililerin toplami

---
+++ baslangic-dotnet-patika/csharp-projects/integer-ikililerin-toplami/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Integer ikilileri girin (örn: 2 3 1 5):");
+        string[] inputs = Console.ReadLine().Split(' ');
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < inputs.Length; i += 2)
+        {
+            if (i + 1 < inputs.Length && int.TryParse(inputs[i], out int first) && int.TryParse(inputs[i + 1], out int second))
+            {
+                numbers.Add(first + second);
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş. Lütfen geçerli bir integer ikilisi girin.");
+                return;
+            }
+        }
+
+        SonuclariYazdir(numbers);
+    }
+
+    static void SonuclariYazdir(List<int> sonuclar)
+    {
+        for (int i = 0; i < sonuclar.Count; i++)
+        {
+            Console.Write(sonuclar[i]);
+
+            if (i < sonuclar.Count - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
